4efd3dbc30798291a7be6bed81983a5f70dec0d9
Stanley T Mah <stmah@acs-cseb260-01.ucsd.edu>
2013-11-16 18:30:32 -0800

    Created Chatroom and ChatRoomDatabase

---
+++ ChatRouletteServer/src/com/group8/database/ChatRoom.java
@@ -0,0 +1,70 @@
+package com.group8.database;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+
+public class ChatRoom {
+	
+	private HashMap<String,User> chatMap;
+	private String name;
+	
+	public ChatRoom(String name, User user)
+	{
+		this.name = name;
+		chatMap = new HashMap<String,User>();
+		chatMap.put(user.getUsername(), user);
+		
+	}
+	
+	public boolean addChatUser(User user)
+	{
+		if(chatMap.containsValue(user.getUsername()))
+			return false;
+		
+		chatMap.put(user.getUsername(), user);
+		return true;
+		
+	}
+	
+	
+	public boolean removeChatUser(User user)
+	{
+		if(!chatMap.containsValue(user.getUsername()))
+			return false;
+		
+		chatMap.remove(user.getUsername());
+		
+		return true;
+		
+	}
+	
+	public String[] getChatUsers()
+	{
+		List<String> chatUsers = new LinkedList<String>();
+		User[] allUsers = chatMap.values().toArray(new User[chatMap.size()]);
+		
+		for( int i = 0; i < allUsers.length; i++ )
+		{
+			if(allUsers[i].isOnline())
+				chatUsers.add( allUsers[i].getUsername() );
+		}
+		return (String[]) chatUsers.toArray(new String[chatUsers.size()]); 
+	}
+	
+	public boolean isChatEmpty()
+	{
+		return chatMap.isEmpty();
+	}
+	
+	public String getChatName()
+	{
+		return name;
+	}
+	
+	public void renameChat(String name)
+	{
+		this.name = name;
+	}
+
+}
---
+++ ChatRouletteServer/src/com/group8/database/ChatRoomDatabase.java
@@ -0,0 +1,61 @@
+package com.group8.database;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+
+public class ChatRoomDatabase {
+	private HashMap<String, ChatRoom> roomMap;
+	
+	public ChatRoomDatabase(){
+		this.roomMap = new HashMap<String, ChatRoom>();
+	}
+	
+	public boolean addChatRoom( String name, User user){
+		if( roomMap.containsKey(name))
+			return false;
+		roomMap.put(name, new ChatRoom(name, user));
+		return true;
+	}
+	
+	public boolean removeChatRoom(String name){
+		if( !roomMap.containsKey(name))
+			return false;
+		roomMap.remove(name);
+		return true;
+	}
+	
+	public boolean renameChatRoom( String current, String newName){
+		if( roomMap.containsKey(current))
+			return false;
+		roomMap.get(current).renameChat(newName);
+		roomMap.put(newName, roomMap.get(current));
+		roomMap.remove(current);
+		return true;
+		
+	}
+	
+	public String[] getChatRoomArray(){
+		List<String> chatRooms = new LinkedList<String>();
+		ChatRoom[] allChatRooms = roomMap.values().toArray(new ChatRoom[roomMap.size()]);
+		
+		for( int i = 0; i < allChatRooms.length; i++ )
+			chatRooms.add( allChatRooms[i].getChatName() );
+
+		return (String[]) chatRooms.toArray(new String[chatRooms.size()]); 
+	}
+	
+	public String listChatRooms(){
+		String chatList = "";
+		String[] chatArrayList = this.getChatRoomArray();
+		for( int i = 0; i < chatArrayList.length; i++)
+			chatList = chatList + chatArrayList[i] + "\n";
+		return chatList;
+	}
+	
+	public ChatRoom getChatRoom( String name){
+		return roomMap.get(name);
+	}
+}
+
+
