7a2114eb9bf7693dc1ba16a78c43174c6e7c147e
hgrewal <h3grewal@gmail.com>
2013-11-19 22:22:55 -0800

    Waitlist (MainGUI) now implements View and gets initialized with ChatClient

--- ChatRouletteClient/src/com/group8/gui/WaitList.java
+++ ChatRouletteClient/src/com/group8/gui/WaitList.java
@@ -1,16 +1,33 @@
 package com.group8.gui;
 
 import javax.swing.*;
+
+import com.group8.client.ChatClient;
+import com.group8.view.View;
+
 import java.awt.*;
  
-public class WaitList {
+public class WaitList implements View {
  
+	static ChatClient client; 
+	public WaitList(ChatClient c)
+	{
+		client = c; 
+		String[] args = null; 
+		main(args); 
+	}
+	
+	public void displayMessage( String msg ){}
+	public void displayInfo(String info){}
+
+
+	
 	private static void createAndShowUI()
 	{ 
-		Student student = new Student();
+		Login login = new Login(client);
         JTabbedPane tabs = new JTabbedPane();
         JFrame frame = new JFrame("ChatRoulette");
-        tabs.add("Login Screen", student);
+        tabs.add("Login Screen", login);
                 
         frame.add(tabs, BorderLayout.CENTER);
         frame.setSize(600,700);
