bf8215dffe61e1d37f143adf45dc1c8427e2a3cb
rchaloux <rchaloux@ucsd.edu>
2013-11-21 21:46:47 -0800

    changed IP

--- ChatRouletteClient/src/com/group8/client/Constants.java
+++ ChatRouletteClient/src/com/group8/client/Constants.java
@@ -1,7 +1,7 @@
 package com.group8.client;
 
 public interface Constants {
-	public static String ACTIVEMQ_URL = "tcp://128.54.70.12:61616";
+	public static String ACTIVEMQ_URL = "tcp://128.54.70.12:61613";
 	public static String USERNAME = "max";	
 	public static String PASSWORD = "pwd";	
 	public static String QUEUENAME = "test";
